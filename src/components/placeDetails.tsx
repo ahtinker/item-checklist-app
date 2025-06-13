dc59b5f00a8ef8c73f1907fb6a79a153a040d1b6
Ahti Ankeriasniemi <ahti.ankeriasniemi@gmail.com>
2025-06-13 09:26:08 +0300

    Update placeDetails.tsx

--- src/components/placeDetails.tsx
+++ src/components/placeDetails.tsx
@@ -22,34 +22,40 @@ const PlaceDetails: React.FC<PlaceDetailsProps> = ({ isActive, id, onClose }) =>
                     <button className="delete" aria-label="close" onClick={onClose}></button>
                 </header>
                 <section className="modal-card-body" style={{padding: "0px", paddingTop: "15px"}}>
-                    {items.map((item: any, index: number) => (
-                        <div key={index} className="pb-1">
-                        <div className='columns pl-5 is-mobile is-vcentered mb-1' style={{width: "100%"}}>
-                            <div className="column is-2">
-                                <span className="is-size-3 pl-3">
-                                {item.emoji.substring(0,2)}
-                                </span>
-                            </div> 
-                            <div className="column">
-                                <div>
-                                <strong>{item.name}</strong>
-                                {item.available ? "" :
-                                  <span className="ml-3 has-text-danger">Kadonnut</span>
-                                }
-                                <br/>
-                                <span className="icon-text">
-                                    <span className="icon">
-                                    <i className="fas fa-location-dot"></i>
+                    {items.length > 0 ? 
+                        items.map((item: any, index: number) => (
+                            <div key={index} className="pb-1">
+                            <div className='columns pl-5 is-mobile is-vcentered mb-1' style={{width: "100%"}}>
+                                <div className="column is-2">
+                                    <span className="is-size-3 pl-3">
+                                    {item.emoji.substring(0,2)}
                                     </span>
-                                    <span>{PlaceStorage.getPlaces(item.placeId)?.name || "Ei paikkaa"}</span>
-                                </span>
-                                
+                                </div> 
+                                <div className="column">
+                                    <div>
+                                    <strong>{item.name}</strong>
+                                    {item.available ? "" :
+                                    <span className="ml-3 has-text-danger">Kadonnut</span>
+                                    }
+                                    <br/>
+                                    <span className="icon-text">
+                                        <span className="icon">
+                                        <i className="fas fa-location-dot"></i>
+                                        </span>
+                                        <span>{PlaceStorage.getPlaces(item.placeId)?.name || "Ei paikkaa"}</span>
+                                    </span>
+                                    
+                                    </div>
+                                    
                                 </div>
-                                
                             </div>
                         </div>
-                      </div>
-                    ))}
+                        ))
+                     : (
+                        <div className="has-text-centered">
+                            <p className="is-size-5">Ei tavaroita</p>
+                        </div>
+                    )}
                 </section>
                 <footer className="modal-card-foot">
                     <div className="buttons">
